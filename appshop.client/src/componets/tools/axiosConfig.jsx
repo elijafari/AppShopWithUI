d102c0495b705bead9f84ae7dcab0f3c258ad3e4
105298234 <105298234@omidtech-co.local>
2025-09-07 07:42:41 +0330

    orange btn

--- appshop.client/src/componets/tools/axiosConfig.jsx
+++ appshop.client/src/componets/tools/axiosConfig.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 console.log("url:" + import.meta.env.VITE_API_URL);
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL+"/api",
-  timeout: 10000,
+  timeout: 30000,
   withCredentials: true   // 👈 اینجا
 });
 
